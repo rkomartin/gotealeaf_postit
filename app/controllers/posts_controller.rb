22ed761165d5904c442ebdb8b5653d6d690f210f
Robert Komartin <robert.komartin@gmail.com>
2013-05-15 07:59:28 +0300

    Adding views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.user_id = 1
+    if @post.save
+      flash[:notice] = "New Post Successfully Created!"
+      redirect_to posts_path
+    end
+  end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+    if @post.nil?
+      flash[:alert] = "This Post Does NOT Exist!"
+      redirect_to posts_path
+    end
   end
 end
