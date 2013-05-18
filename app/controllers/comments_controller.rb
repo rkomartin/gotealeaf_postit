feece5ee5aecda2655186ba7b67fab6c66a43a3a
Robert Komartin <robert.komartin@gmail.com>
2013-05-18 16:15:28 +0300

    Forms improvements and completions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,10 +11,10 @@ class CommentsController < ApplicationController
     @comment.post_id = params[:post_id]
     @comment.user_id = 1 #TODO Use user_id from authentication
     if @comment.save
-      flash[:notice] = "New Comment Successfully Created!"
+      flash[:notice] = "New comment successfully created!"
       redirect_to post_comments_path
     else
-      flash[:alert] = "Validation Errors with this Comment!"
+      flash[:alert] = "Comment can't be blank!"
       redirect_to new_post_comment_path(params[:post_id])
     end
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,24 +4,38 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(params[:post])
     @post.user_id = 1 #TODO Use user_id from authentication
     if @post.save
-      flash[:notice] = "New Post Successfully Created!"
+      flash[:notice] = "New post successfully created!"
       redirect_to posts_path
     else
-      flash[:alert] = "Validation Errors with this Post!"
-      redirect_to new_post_path
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Post was successfully updated!"
+      redirect_to post_path(@post.id)
+    else
+      render :edit
     end
   end
 
   def show
     @post = Post.find_by_id(params[:id])
     if @post.nil?
-      flash[:alert] = "This Post Does NOT Exist!"
+      flash[:alert] = "This post does NOT exist!"
       redirect_to posts_path
     end
   end
